44a1ebdf7071ef66a8649f33445e8e05ce594bb9
llilliililli <wnsrn403@naver.com>
2023-08-23 10:26:44 +0900

    WebCrowling Test With Naver

---
+++ webCrowlingNaver.py
@@ -0,0 +1,84 @@
+# 크롤링 1 ) BeautifulSoup 방식
+
+from urllib.request import urlopen
+import requests
+from bs4 import BeautifulSoup
+import time
+
+
+# 크롤링 로그인( 세션 )
+
+LOGIN_NAVER = "https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/"
+NAVER = "https://www.naver.com/"
+
+id = '네이버 계정'
+pw = '네이버 계정 암호'
+
+
+session = requests.Session()
+
+login_info = {
+    "id":id,
+    "pw":pw
+}
+
+
+
+with session as s:
+
+    firstPage = s.get(LOGIN_NAVER)
+    html = firstPage.text
+    soup = BeautifulSoup(html, "html.parser")
+    locale = soup.find('input',{'name':'locale'})
+    timezone = soup.find('input',{'name':'timezone'})
+    sysId = soup.find('input',{'name':'sysId'})
+    csrf = soup.find('input',{'name':'_csrf'})
+
+    print('locale :: '+str(locale['value']))
+    print('timezone :: '+str(timezone['value']))
+    print('sysId :: '+str(sysId['value']))
+    print('csrf :: '+str(csrf['value']))
+
+    headers = {  # 참조 및 유저정보 입력
+        'Referer': LOGIN_NAVER,
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+    }
+
+    # 크롬 개발자도구 > 네트워크 > LOGIN_NAVER URL > 페이로드 양식 데이터 
+    LOGIN_INFO = { **{'locale': locale['value']}, **{'timezone': timezone['value']}, **{'sysId': sysId['value']},**{'_csrf': csrf['value']}, **login_info}
+
+    print('LOGIN_INFO :: '+str(LOGIN_INFO))
+
+    response = session.post(LOGIN_NAVER,data=LOGIN_INFO, headers=headers)
+
+    # APROS 로그인 처리
+    if response.status_code == 200:
+        print('로그인 성공')
+        time.sleep(2)
+
+        #print(response.text)
+        
+        request = s.get(NAVER,headers=headers)
+
+        print(request.text)
+
+    else:
+        print('로그인 실패')
+
+
+
+# 크롤링
+
+
+# html = urlopen(APROS)
+
+
+#print(bsObject)
+#print(bsObject.title)
+#print(bsObject.head.find("meta",{"name":"description"}))
+# print(bsObject.body.find("div",{"class":"login_area"}))
+# time.sleep(1)
+
+
+
+# print(response.text)
---
+++ webCrowlingNaver2.py
@@ -0,0 +1,79 @@
+# 크롤링 2 ) selenium 방식
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import pyperclip
+
+
+LOGIN_NAVER = "https://nid.naver.com/nidlogin.login?mode=form&url=https://www.naver.com/"
+NAVER = "https://www.naver.com/"
+
+id = '네이버 계정'
+pw = '네이버 계정 암호'
+
+
+driver = webdriver.Chrome()
+driver.get(LOGIN_NAVER)
+# print(driver.body)
+time.sleep(1)
+
+tag_Id = driver.find_element(By.ID,'id')
+tag_Pw = driver.find_element(By.ID,'pw')
+
+tag_Id.clear()
+tag_Pw.clear()
+time.sleep(1)
+
+# # id 입력
+tag_Id.click()
+pyperclip.copy(id)
+tag_Id.send_keys(Keys.CONTROL,'v')
+time.sleep(1)
+
+# # pw 입력
+tag_Pw.click()
+pyperclip.copy(pw)
+tag_Pw.send_keys(Keys.CONTROL,'v')
+time.sleep(1)
+
+
+# #login Btn
+login_btn = driver.find_element(By.CLASS_NAME,'btn_login')
+login_btn.click()
+time.sleep(1)
+
+
+# #new.dontsave
+login_btn = driver.find_element(By.ID,'new.dontsave')
+login_btn.click()
+time.sleep(2)
+
+
+# try:
+
+#     # btns = driver.find_elements(By.TAG_NAME,'sc-button')
+#     # for b in btns:
+#     #     print(b.text)
+
+#     # alert = driver.switch_to.alert
+#     # print(alert.text)
+#     # alert.accept
+
+#     # driver.find_element(By.LINK_TEXT, "확인").click()
+#     # text = alert.text
+#     # alert.accept()
+
+#     tap_btn = driver.find_element(By.TAG_NAME,'sc-toolbar')
+#     print(tap_btn.text)
+#     #tap_btn.click()
+
+
+# except:
+#     print('pass')
+#     pass
+
+# # popup_btn = driver.find_element(By.CLASS_NAME,'style-scope sc-messagebox')
+# # popup_btn.click()
+# time.sleep(5)
